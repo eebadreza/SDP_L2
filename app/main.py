acec9ad02249acd1edeb3f8262b26ae99922a9df
Eebad Reza <95500079+eebadreza@users.noreply.github.com>
2024-06-12 20:31:40 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from util import classify, set_background
 import tensorflow as tf
 
 # set_background('blk.jpeg')
-print(os.getcwd())
+# print(os.getcwd())
 # set title
 st.title('Diabetic Retinopathy classification')
 # set header
@@ -18,8 +18,21 @@ file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 # load classifier
 # model = tf.keras.models.load_model('/Users/apple/Downloads/SDP/pretrained_models/B/model_weighted_avg.keras', custom_objects=dict(CustomLayer=CustomLayer))
 # model = tf.keras.models.load_model('/Users/apple/Downloads/SDP/pretrained_models/B/model_weighted_avg.keras', custom_objects={ "TransformerEncoderLayer": TransformerEncoderLayer, "PositiionalEmbedding": PositiionalEmbedding, "TransformerDecoder": TransformerDecoder})
+################################
+model_path = os.path.join(os.path.dirname(__file__), '..', 'pretrained_models', 'B', 'model_mobilenet.keras')
 
-model = tf.keras.models.load_model('../pretrained_models/B/model_mobilenet.keras')
+# Normalize the path to avoid issues with different operating systems
+model_path = os.path.normpath(model_path)
+print("Full path to the model file:", model_path)
+
+# Check if the model file exists
+if not os.path.isfile(model_path):
+    raise FileNotFoundError(f"Model file not found: {model_path}")
+
+# Load the model
+model = tf.keras.models.load_model(model_path)
+################################
+# model = tf.keras.models.load_model('../pretrained_models/B/model_mobilenet.keras')
 
 # load class names
 with open('./model/labels.txt', 'r') as f:
